ffd4dcbc25b8bc0fa628aa762f780c0d75135903
Alex Burka <aburka@seas.upenn.edu>
2015-08-04 14:26:44 -0400

    stu merged in the parking lot code

--- nri_daq/nri_daq.ino
+++ nri_daq/nri_daq.ino
@@ -1,45 +1,46 @@
 #include <IntervalTimer.h>
 #include <SPI.h>
 
-#include "TimerObject.h"
-TimerObject *timer1 = new TimerObject(12);
-
-
 #include "Wire.h"
 #include <math.h>
 
-// I2Cdev and MPU9150 must be installed as libraries, or else the .cpp/.h files
-// for both classes must be in the include path of your project
-#include "I2Cdev.h"
-#include "MPU9150.h"
-#include "helper_3dmath.h"
+#include "TimerObject.h"
+TimerObject *timer1 = new TimerObject(1);
 
-// class default I2C address is 0x68
-// specific I2C addresses may be passed as a parameter here
-// AD0 low = 0x68 (default for InvenSense evaluation board)
-// AD0 high = 0x69
-MPU9150 accelGyroMag;
-uint8_t ajx, ajy, ajz;
+#include "I2Cdev.h"
+#include "LSM303DLHC.h"
+#include "L3GD20H.h"
+
+LSM303DLHC accelMag;
+L3GD20H gyro;
+
+
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 int16_t mx, my, mz;
 
-uint8_t buffer_accel[6];
-uint8_t buffer_gyro[6];
+uint8_t buffer_accel[50];
+uint8_t buffer_gyro[50];
 uint8_t buffer_mag[6];
 
 
-
-int16_t mx_max,  mx_min, my_max, my_min, mz_max, mz_min;
-
-float mx_off, mx_sca, my_off, my_sca, mz_off, mz_sca;
+uint8_t number_gyro;
+uint8_t number_accel;
+bool imuReady = 0;
 
 #define RESET       0x10
+
+#define opto      0
+#define stick     1
+#define biotac    2
+byte PS_Status =  0;
 
 #define LED_1     4
 #define LED_2     3
 #define LED_3     6
 #define LED_4     5
+
+#define PS_request    0x04
 
 #define SETUP_ACC     0x64
 #define CONV_ACC    0xC0
@@ -49,7 +50,7 @@ float mx_off, mx_sca, my_off, my_sca, mz_off, mz_sca;
 #define CONV_FT     0xD8
 #define CS_FT     9
 
-#define SAMPLE_RATE   500 // Delay in us between samples
+#define SAMPLE_RATE   333 // Delay in us between samples
 
 #define SCAN_MODE00   0x0
 #define SCAN_MODE01   0x1
@@ -70,12 +71,27 @@ float mx_off, mx_sca, my_off, my_sca, mz_off, mz_sca;
 #define UNIPOLAR  0x2
 #define BIPOLAR   0x3
 
-#define START 0x01
-#define STOP 0x02
-#define PING 0x03
-#define CALI 0x00
+#define START '1' 
+#define STOP '2'
+#define PING '3'
+#define REQUEST_PS '4'
 
 IntervalTimer irq;
+
+void readIMU(){
+
+  number_accel = accelMag.getAccelFIFOStoredSamples();
+  for (byte i=0; i<number_accel; i++){
+    accelMag.getAccelByte(&buffer_accel[6*i]);
+  }
+  accelMag.getMagByte(buffer_mag);
+  number_gyro = gyro.getFIFOStoredDataLevel();
+  for (byte i=0; i<number_gyro; i++){ 
+    gyro.getGyroByte(&buffer_gyro[6*i]);
+  }
+  imuReady = 1;
+
+}
 
 
 byte convReg(byte channel, byte scan_mode, byte temp)
@@ -92,23 +108,33 @@ byte setupReg(byte cksel, byte refsel, byte diffsel)
 
 void readADC(void)
 {
-  
+
   static byte j = 0;
   static byte data[50];
   byte i = 0;
-  static long int pre = 0;
 
-  j++;
   
+  j++;
+
 
   Serial.flush();
-
-  Serial.write(buffer_accel, 6);
-  Serial.write(buffer_gyro, 6);
-  Serial.write(buffer_mag, 6);
+  Serial.write("aaa");
   
+  
+  if (imuReady==1){
+    Serial.write(33+6*(number_accel+number_gyro+1));
+    Serial.write(number_accel); Serial.write(number_gyro);
+    Serial.write(buffer_accel, 6*number_accel);
+    Serial.write(buffer_gyro,  6*number_gyro);
+    Serial.write(buffer_mag,   6);
+    imuReady = 0;
+  }
+  else{
+    Serial.write(31);
+  }
+
   for (i = 0; i < 12; i++)
-  {
+  { 
     digitalWrite(CS_FT, LOW);
     data[i] = SPI.transfer(0x00);
     digitalWrite(CS_FT, HIGH);
@@ -116,7 +142,7 @@ void readADC(void)
   }
 
   for (i = 12; i < 30; i++)
-  {
+  { 
     digitalWrite(CS_ACC, LOW);
     data[i] = SPI.transfer(0x00);
     digitalWrite(CS_ACC, HIGH);
@@ -126,9 +152,7 @@ void readADC(void)
   data[30] = j;
 
   Serial.write(data, 31);
-  //  Serial.print(' ');
-
-
+  
   digitalWrite(CS_FT, LOW);
   SPI.transfer(CONV_FT);
   digitalWrite(CS_FT, HIGH);
@@ -136,33 +160,7 @@ void readADC(void)
   digitalWrite(CS_ACC, LOW);
   SPI.transfer(CONV_ACC);
   digitalWrite(CS_ACC, HIGH);
-  
 
-
-
-//  Serial.print(ax);
-//  Serial.print('\t');
-//  Serial.print(ay);
-//  Serial.print('\t');
-//  Serial.print(az);
-//  Serial.print('\t');
-//  Serial.print(gx);
-//  Serial.print('\t');
-//  Serial.print(gy);
-//  Serial.print('\t');
-//  Serial.print(gz);
-//  Serial.print('\t');
-//  Serial.print((mx-mx_off)/mx_sca);
-//  Serial.print('\t');
-//  Serial.print((my-my_off)/my_sca);
-//  Serial.print('\t');
-//  Serial.print((mz-mz_off));
-//  Serial.print('\t');
-//  Serial.print(atan2((mx-mx_off)*mx_sca, (my-my_off)*my_sca));
-//  Serial.print(' ');
-//  Serial.println(micros() - pre);
-//  pre = micros();
-  
 
 }
 void pulseCS(char pin)
@@ -196,87 +194,25 @@ void setupACC(void)
   digitalWrite(CS_ACC, HIGH);
 }
 
-
-void readIMU(){
-
-  accelGyroMag.getAccelByte(buffer_accel);
-  accelGyroMag.getGyroByte(buffer_gyro);
-  accelGyroMag.getMagByte(buffer_mag);
-
+void parkingSpot(void)
+{
+  //if the status of the parking spot is requested this function is called
+  if(digitalRead(opto)==HIGH)PS_Status=PS_Status   |1;
+  if(digitalRead(stick)==HIGH)PS_Status=PS_Status  |2;
+  if(digitalRead(biotac)==HIGH)PS_Status=PS_Status |4;
+  Serial.write(PS_Status);
+  PS_Status = 0;
 }
-
-void calibrateCompass(float* mx_off, float* mx_sca, float* my_off, float* my_sca, float* mz_off, float* mz_sca){
-  Serial.println(F("Calibrating compass. Send any key to abort."));
-  Serial.flush();
-  static long int pp;
-
-//  accelGyroMag.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  accelGyroMag.getMagnetometer(&mx, &my, &mz);
-  
-
-  mx_max = mx;
-  mx_min = mx-1;
-  my_max = my;
-  my_min = my-1;
-  mz_max = mz;
-  mz_min = mz-1;
-  while(1) {
-//    
-    pp = micros();
-    accelGyroMag.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-    accelGyroMag.getMagnetometer(&mx, &my, &mz);
-    Serial.print(micros()-pp); Serial.print(' ');
-    Serial.print(mx); Serial.print(' ');
-    Serial.print(my); Serial.print(' ');
-    Serial.println(mz);
-    if (mx>mx_max){
-      mx_max = mx;
-    }
-    else if (mx<mx_min){
-      mx_min = mx;
-    }
-    if (my>my_max){
-      my_max = my;
-    }
-    else if (my<my_min){
-      my_min = my;
-    }
-    if (mz>mz_max){
-      mz_max = mz;
-    }
-    else if (mz<mz_min){
-      mz_min = mz;
-    }
-  
-    delay(10);
-  
-  
-    if (Serial.available()){
-      
-      *mx_off = (mx_max+mx_min)/2.0;
-      *my_off = (my_max+my_min)/2.0;
-      *mz_off = (mz_max+mz_min)/2.0;
-      *mx_sca = (mx_max-mx_min) * 1.0 / (mz_max-mz_min);
-      *my_sca = (my_max-my_min) * 1.0 / (mz_max-mz_min);
-      *mz_sca = 1.0;
-      Serial.flush();
-      Serial.print(mz_min); Serial.print(' ');
-      Serial.print(mz_max); Serial.print(' ');
-      Serial.print(*mx_off); Serial.print(' ');
-      Serial.print(*mx_sca); Serial.print(' ');
-      Serial.print(*my_off); Serial.print(' ');
-      Serial.print(*my_sca); Serial.print(' ');
-      Serial.print(*mz_off); Serial.print(' ');
-      Serial.print(*mz_sca); Serial.print(' ');
-      return;
-    }
-  }
-}
-
 
 void setup(void)
 {
 
+  //Declare parking spot pins as input
+  
+  pinMode(opto,INPUT);
+  pinMode(stick,INPUT);
+  pinMode(biotac,INPUT);
+  
   // Turn on LEDS (only LED_4 connected)
 
   pinMode(LED_1, OUTPUT);
@@ -290,7 +226,7 @@ void setup(void)
 
   // Start USB
   Serial.begin(115200);
-  while (!Serial);
+  
 
   // Declare chip select pins, set to idle high
   pinMode(CS_ACC, OUTPUT);
@@ -303,30 +239,35 @@ void setup(void)
   SPI.setClockDivider(SPI_CLOCK_DIV2);
   SPI.setDataMode(SPI_MODE0);
 
-  Wire.begin();
-  TWBR = 12;
-  accelGyroMag.initialize();
-  accelGyroMag.setDLPFMode(MPU9150_DLPF_BW_20);
-  accelGyroMag.setFullScaleGyroRange(MPU9150_GYRO_FS_500);
-  accelGyroMag.setFullScaleAccelRange(MPU9150_ACCEL_FS_4);
-  
-  accelGyroMag.getAdj(&ajx, &ajy, &ajz);
+   Wire.begin();
+  accelMag.initialize();
+  accelMag.setAccelFullScale(4);
+  accelMag.setAccelBlockDataUpdateEnabled(true);
+  accelMag.setAccelHighResOutputEnabled(true);
+  accelMag.setAccelOutputDataRate(1344);
+  accelMag.setMagOutputDataRate(220);
+  accelMag.setMagGain(230);
+  accelMag.setMagMode(LSM303DLHC_MD_CONTINUOUS);
 
+
+  gyro.initialize();
+  gyro.setFullScale(500);
+  gyro.setBlockDataUpdateEnabled(true);
+  gyro.setOutputDataRate(800);
+  gyro.setBandwidthCutOffMode(L3GD20H_BW_MED_LOW);
+
+   
   timer1->setOnTimer(&readIMU);
-  
-
 }
-
 
 void loop(void)
 {
-  //  Serial.println("asdasd");
+//  Serial.println("asdasd");
   byte message[5];
   byte sample_rate_buff[2];
   int sample_rate;
   byte packet_length;
   timer1->Update();
-  
 
   if (Serial.available())
   {
@@ -340,15 +281,11 @@ void loop(void)
 
     Serial.flush();
 
-    if (message[0] == CALI){
-      calibrateCompass(&mx_off, &mx_sca, &my_off, &my_sca, &mz_off, &mz_sca);
-    }
     if (message[0] == START)
     {
-      
       if (packet_length > 1)
       {
-        sample_rate = 1000000 / ((int)(message[1] << 8) + (int)message[2]);
+        sample_rate = 1000000/((int)(message[1]<<8) + (int)message[2]);
         // delay(1);
       }
 
@@ -356,34 +293,45 @@ void loop(void)
       {
         sample_rate = SAMPLE_RATE;
       }
-
+      
       // delay(1);
       setupACC();
       setupFT();
-
+      
       delayMicroseconds(sample_rate);
-
+      accelMag.setAccelFIFOEnabled(false);
+      accelMag.setAccelFIFOMode(LSM303DLHC_FM_BYBASS);
+      accelMag.setAccelFIFOMode(LSM303DLHC_FM_STREAM);
+      accelMag.setAccelFIFOEnabled(true);
+      accelMag.rebootAccelMemoryContent();
+      gyro.setFIFOEnabled(false);
+      gyro.setFIFOMode(L3GD20H_FM_BYPASS);
+      gyro.setFIFOMode(L3GD20H_FM_STREAM);
+      gyro.setFIFOEnabled(true);
+      gyro.rebootMemoryContent();
       irq.begin(readADC, sample_rate);
       timer1->Start();
-      
     }
 
     if (message[0] == STOP){
       irq.end();
       timer1->Stop();
-      
-      
     }
 
     if (message[0] == PING)
-    {
-      irq.end();
-      Serial.flush();
-      Serial.write(0x01);
-    }
-
+      {
+        irq.end();
+        Serial.flush();
+        Serial.write(0x01);
+      }
+    
+    if (message[0] == REQUEST_PS)
+      {
+        parkingSpot();
+      }
+    
     if (!Serial.dtr())
-      irq.end();
+      irq.end();  
     interrupts();
 
   }
